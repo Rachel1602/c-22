8729a642c8dbc969f292dae1b2a548554edd36f1
Rachel1602 <69745569+Rachel1602@users.noreply.github.com>
2020-09-13 19:00:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+//to use the physics engine- 3 things
+//Engine,World,Bodies
+
+//to create the physics engine
+const Engine=Matter.Engine;
+
+//to create the physical world
+const World=Matter.World;
+
+//to create the physical grounds in our world
+const Bodies=Matter.Bodies;             
+
+var engine,world; 
+var ground;
+var ball;
+
+function setup() {
+  var canvas=createCanvas(400,400);
+  engine=Engine.create();
+  world=engine.world;
+
+  var ground_options={
+    isStatic:true
+  }
+
+  ground=Bodies.rectangle(200,390,200,20,ground_options);
+  World.add(world,ground);
+  console.log(ground);
+  console.log(ground.position.x);
+  console.log(ground.position.y);
+var ball_options={
+  restitution:1
+}
+  ball=Bodies.circle(200,100,20,ball_options);
+  World.add(world,ball);
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+  rectMode(CENTER)
+  rect(ground.position.x,ground.position.y,400,20);  
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,20,20);
+}
